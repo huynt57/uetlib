e46182ff83c713b7e7a255cff1131473d4d9b81e
Nguyễn Thế Huy <huynt57@gmail.com>
2015-02-14 01:17:28 +0700

    thêm icon

--- protected/views/search/detailuser.php
+++ protected/views/search/detailuser.php
@@ -1,5 +1,5 @@
 <div class="panel" style="margin-left: 10px;">
-    <h6>Mã số sinh viên: <?php echo $studentID ?></h6>
+    <h6><i class="icon-user"></i> Mã số sinh viên: <?php echo $studentID ?></h6>
     <h6><i class="icon-books"></i> Số sách đang mượn: <?php echo count($student_detail) ?> quyển</h6>
     <?php if (count($student_detail) != 0): ?>
         <h6><i class="icon-paragraph-justify2"></i> Chi tiết sách đang mượn: </h6>
